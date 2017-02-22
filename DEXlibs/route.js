97ef7b1cb861cc34544005ef2c85c67847911951
desarrollovba <desarrollovba@gmail.com>
2017-02-22 04:21:34 -0500

    Many changes for new functions

--- DEXlibs/route.js
+++ DEXlibs/route.js
@@ -1,47 +1,87 @@
 var ws=require('ws');
+var transfer=require('./transfer.js');
 const vwssserver = require('../config.js').ws
 
 module.exports = function(objinput,callback) {
   //console.log(objinput);
    switch(objinput.fname) {
-        case "get_account_balances":
-             
-              // var temp2= { "id": 0,"method": "call", "params": [0, "get_account_balances",[ objinput.vaccount ,[]]]}
-             console.log(" ========= split " ,objinput.vaccount.split(' ') );
-       var temp2= { "id": 0,"method": "call", "params": [0, "get_account_balances",objinput.vaccount.split(' ')]}
-                 console.log('gaccb---',temp2);
-               break;
+        case "get_full_accounts":
+     
+            temp2= { "id": 0,"method": "call", "params": [0, "get_full_accounts",[objinput.vaccount.split(' '),'false']]};
+            console.log('---faccounts---',temp2); 
+            break;
       
         case "get_named_account_balances":
-        console.log(" ========= split " ,objinput.vaccount.split(' ') );
-                // var temp2= { "id": 0,"method": "call", "params": [0, "get_account_balances",['1.2.153554',['1.3.1','1.3.0']]]}
-               var temp2= { "id": 0,"method": "call", "params": [0, "get_named_account_balances",objinput.vaccount.split(' ')]}
-                var temp2= { "id": 0,"method": "call", "params": [0, "get_named_account_balances",["op3nalf"]]}
-                break;
+               
+             var temp2= { "id": 0,"method": "call", "params": [0, "get_full_accounts",['somebodyn1ce','somebodyn1ce'],['false']]}; 
+             console.log('----get_named_account_balances--',temp2);                   
+             break;
+       
         case "list_assets":
-             var temp2= { "id": 0,"method": "call", "params": [0, "list_assets",[10,10]]}
+             var temp2= { "d": 0,"method": "call", "params": [0, "list_assets",[10,10]]}   
               break;
 
-         case "lookup_accounts":
+         case "lookup_accounts": 
                var temp2= { "id": 0,"method": "call", "params": [0, "lookup_accounts",['op3nalf',1]]}
                var temp2= { "id": 0,"method": "call", "params": [0, "lookup_accounts",[objinput.vaccount,1]]}
                 break;  
+        case "lookup_accounts_name": 
+               var temp2= { "id": 0,"method": "call", "params": [0, "lookup_accounts",['op3nalf',1]]}
+               var temp2= { "id": 0,"method": "call", "params": [0, "lookup_account_names",[objinput.vobject.split(' ')]]};
+               console.log('---------',temp2);
+                break;   
+       
+         case "get_ticker":
+                var aux=objinput.vquote.split(' ') 
+            //    var temp2= { "id": 2,"method": "call", "params": [0, "get_ticker",[ '1.3.1', '1.3.0' ]]};
+                 var temp2= { "id": '2',"method": "call", "params": [0, "get_ticker",aux]}; 
+                console.log('------get_ticker---', temp2);
+                break;   
 
         case "get_account_by_name":
                var temp2= { "id": 0,"method": "call", "params": [0, "get_account_by_name",[objinput.vaccount.split()]]}
                 break;  
-       case "get_objects":
-       console.log('param  ----- ' ,objinput.vobject);
        
-                console.log(" ========= split " ,objinput.vobject.split(' ') );
+       case "get_objects":
+          
+                console.log(" ========= get_objects " ,objinput.vobject.split(' ') );
              var temp2= { "id": 0,"method": "call", "params": [0, "get_objects", [ objinput.vobject.split(' ') ]]};
           //    var temp2= { "id": 0,"method": "call", "params": [0, "get_objects", [["1.3.1044","1.3.888" ]]]};
       
                 break;  
+         case "get_account_history": 
+          
+          //      console.log(" ========= get_account_history " ,objinput.vobject.split(' ') );
+             var temp2= { "id": '',"method": "call", "params": [3, "get_account_history", ['1.2.153811',100,100,100]]};
+            break;
+       
+       
+       
+       
       
         case "transfer":
+                 
+               var objTx = {
+                privKey: objinput.privKey , //'5JJpm1G7NGUQoLaanMy6qRVD7N5edWWcE1nMTG6eLo6SnTYVBjD',
+                fromAccount: objinput.fromAccount,// 'someoneveryn3w',
+                memo: objinput.memo, //'DSDDFDF',
+                toAccount: objinput.toAccount,//'somebodyn1ce',
+                amount: objinput.amount,//100000,
+                asset:objinput.asset //'BTS'
+                } 
+              
+              
                // var test=transac('ẗransfer')  ;
-                console.log(test) ;break;  
+              transfer(objTx, function(err, tx){
+                if(err){
+                  callback(err, null)
+                }
+                else{
+                  callback(null, tx);
+                }
+              });
+              //console.log(test) 
+              ;break;  
       
       
          default:
@@ -60,7 +100,7 @@ module.exports = function(objinput,callback) {
                   wss.terminate();
                  if(v.error){ console.log(v.error.message);callback(v,null)}
                  else { console.log('---------------------------- received');
-                       console.log(data);
+                      // console.log(data);
                         callback(null, JSON.parse(data))
                       }
 
