db2793314ec1c6da4c4192171ebb513ea0ece6d5
luisan00 <luisan00@MacBook-Pro-de-Luis.local>
2017-03-01 23:03:09 +0100

    Added config.js file

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * config.js for bitsharesAPINode
+ * (c) 2017 TheSerranos.
+ * This code is released under the 
+ * terms of the MIT license. 
+ */
+
+var config = {};
+// Secure WebSocket settings
+// < - - >
+// Change it to:
+// Germany; 'wss://bitshares.openledger.info/ws'
+// Hong Kong; 'wss://bit.btsabc.org/ws'
+// Hong Kong; 'wss://openledger.hk/ws'
+// Hangzhou, China; 'wss://bts.transwiser.com/ws'
+// Hangzhou, China; 'wss://bitshares.dacplay.org:8089/ws'
+// Toronto, Canada; 'wss://secure.freedomledger.com/ws'
+// < - - > 
+// Public testnet; 'wss://testnet.bitshares.eu/ws'
+config.ws = {
+    host: 'wss://node.testnet.bitshares.eu',
+};
+// api listen on port;
+config.api = {
+    port: 3333
+};
+
+module.exports = config;
