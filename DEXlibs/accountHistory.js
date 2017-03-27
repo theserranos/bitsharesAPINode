bdbc3f5f131a79da81da1e16a58a0742db1214d4
luisan00 <luisan00@hotmail.com>
2017-03-27 21:23:11 +0200

    Create accountHistory.js

---
+++ DEXlibs/accountHistory.js
@@ -0,0 +1,10 @@
+/**
+ * accountHistory.js
+ * Get the recent history from a given account.
+ */
+ 
+var {
+    Apis
+} = require("bitsharesjs-ws")
+
+var { ChainStore, FetchChain} = require("bitsharesjs");
