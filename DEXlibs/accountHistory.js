f09f718a157750a9e690078c648b46ffbf40e60a
luisan00 <luisan00@hotmail.com>
2017-03-27 21:30:22 +0200

    Update accountHistory.js

--- DEXlibs/accountHistory.js
+++ DEXlibs/accountHistory.js
@@ -5,3 +5,39 @@
  
 var { Apis } = require("graphenejs-ws");
 var { ChainStore, FetchChain} = require("graphenejs-lib");
+var host = require('../config.js').ws.host;
+
+/**
+ * @param  {JSON}   	  data     	[]
+ * @param  {Function} 	callback 	[]
+ * @return {JSON}            		  []
+ */
+module.exports = function(data, callback) {
+	// @private {String} 	data.account
+	// @private {Number}	data.limit
+    this.accountName = data.account;
+    this.limit = data.limit;
+    this.host = host;
+    this.apis = Apis.instance(this.host, true);
+    this.chain = ChainStore;
+    //
+    this.apis.init_promise
+        .then(() => {
+            this.chain.init()
+        })
+        .then(() => {
+        	return FetchChain("getAccount", this.accountName)
+        })
+        .then((account)=>{
+        	var accountID = (account.toJSON()).id;
+        	return  FetchChain("fetchRecentHistory", accountID)
+        })
+        .then((history)=>{
+        	callback(null, history);
+        })
+        .catch((err) => {
+            console.log(err);
+            callback(err, null);
+        })
+
+};
